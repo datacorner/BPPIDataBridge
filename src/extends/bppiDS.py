95990624f92e11215e3e765c56c4f49b6930fb46
datacorner <benoit@datacorner.fr>
2023-09-30 18:39:14 +0200

    upds

---
+++ src/extends/bppiDS.py
@@ -0,0 +1,45 @@
+__author__ = "datacorner.fr"
+__email__ = "admin@datacorner.fr"
+__license__ = "MIT"
+
+from datasources.csvFileDS import csvFileDS 
+import pipelite.constants as C
+import importlib.resources
+from  extends.bppi.bppiRepository import bppiRepository
+
+CFGFILES_DSOBJECT = "bppiDS.json"
+CFGPARAMS_SERVER = "server"
+CFGPARAMS_TOKEN = "token"
+CFGPARAMS_TABLE = "table"
+
+class bppiDS(csvFileDS):
+
+    @property
+    def parametersValidationFile(self):
+        filename = importlib.resources.files(C.RESOURCE_PKGFOLDER_DATASOURCES).joinpath(CFGFILES_DSOBJECT)
+        return str(filename)
+    
+    def initialize(self, cfg) -> bool:
+        """ initialize and check all the needed configuration parameters
+        Args:
+            cfg (objConfig) : params for the data source.
+                example: {'separator': ',', 'filename': 'test2.csv', 'path': '/tests/data/', 'encoding': 'utf-8'}
+        Returns:
+            bool: False if error
+        """
+        try:
+            self.server = cfg.getParameter(CFGPARAMS_SERVER, C.EMPTY)
+            self.token = cfg.getParameter(CFGPARAMS_TOKEN, C.EMPTY)
+            self.table = cfg.getParameter(CFGPARAMS_TABLE, C.EMPTY)
+            return True
+        except Exception as e:
+            self.log.error("{}".format(e))
+            return False
+
+    def load(self) -> int:
+        # Initialize repository
+        bppiRepo = bppiRepository(log=self.log)
+        bppiRepo.initialize(server=self.server, token=self.token)
+        # load data files
+    
+        return 0
